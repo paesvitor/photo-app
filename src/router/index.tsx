333899b3a8deba57f1d560da3f82ec434caadd14
Vitor Paes <paesvitor@outlook.com>
2020-04-01 07:21:38 -0300

    chore: Update Router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, HashRouter } from "react-router-dom";
 import AlbumsScreen from "screens/Albums";
 import AlbumScreen from "screens/Album";
 
 // Components
 function Router() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={AlbumsScreen} />
         <Route exact path="/album/:id" component={AlbumScreen} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
