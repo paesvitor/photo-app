1492abdecb5f831fabf428e4ca4255c08b52930d
Vitor Paes <paesvitor@outlook.com>
2020-04-01 07:10:24 -0300

    Updates

---
+++ precache-manifest.8cc9dc3ea2f96c7d5591e15278da14e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcc26f63e97a47980b0f3189bb9a9ce",
+    "url": "/photo-app/index.html"
+  },
+  {
+    "revision": "ea401b0d86c124dc5984",
+    "url": "/photo-app/static/js/2.f4fd9986.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/photo-app/static/js/2.f4fd9986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525242ac06a10a76be33",
+    "url": "/photo-app/static/js/main.ce29d08b.chunk.js"
+  },
+  {
+    "revision": "8a5b03e4cf2804819908",
+    "url": "/photo-app/static/js/runtime-main.2ad4478a.js"
+  }
+]);
